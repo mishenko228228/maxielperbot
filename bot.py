dc458d031c6857c22c6b19357bcf7b2889a5647e
mishenko228228 <tect.111111@gmail.com>
2025-05-05 14:48:52 -0700

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,19 @@
+from telegram.ext import Updater, CommandHandler
+import os
+
+TOKEN = os.environ.get('TOKEN')
+
+def start(update, context):
+    update.message.reply_text('Привет! Я MaxHelperBot.')
+
+def main():
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler('start', start))
+
+    updater.start_polling()
+    updater.idle()
+
+if name == '__main__':
+    main()
