9cb41acc3fe7653db048a36d4d96e3e88b66134f
mishenko228228 <tect.111111@gmail.com>
2025-05-05 16:43:32 -0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ def main():
 
     dp.add_handler(CommandHandler('start', start))
 
-    PORT = int(os.environ.get('PORT', 8443))
+    PORT = 8443
 
     updater.start_webhook(
         listen='0.0.0.0',
